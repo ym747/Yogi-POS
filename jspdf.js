28cbcae7776aa8cfd5176dce3f8134344e3d029a
Yugendran Mahendran <ymahendran747@gmail.com>
2025-04-18 00:17:07 +0800

    Update jspdf.js

--- jspdf.js
+++ jspdf.js
@@ -1,4 +1,6 @@
   <script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+
     let cart = [];
 
     document.getElementById("product-form").addEventListener("submit", function(e) {
